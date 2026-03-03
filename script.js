fd7ff13862f82429ac89270d541a2062617f0523
Huldah <huldahtshibangu7@gmail.com>
2026-03-03 09:40:34 +0300

    This is the project task

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Welcome to Mwanga Construction Company!");
+}
+
+function validateForm() {
+    alert("Thank you for contacting Mwanga Construction!");
+    return true;
+}
